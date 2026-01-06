eca8c103ba7de80cb04442956992e8bb30bf2983
tule5 <tu.le5@mservice.com.vn>
2026-01-06 11:32:59 +0700

    Fix: Improve LazyVim install check (verify .git existence)

--- run_onchange_install_lazyvim.sh
+++ run_onchange_install_lazyvim.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-if [ ! -d "$HOME/.config/nvim" ]; then
+if [ ! -d "$HOME/.config/nvim/.git" ]; then
+    # If the directory exists but isn't a git repo, remove it to allow a clean clone.
+    # Chezmoi will ensure managed files (like user_configs.lua) are applied/restored.
+    rm -rf "$HOME/.config/nvim"
     git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
 fi
